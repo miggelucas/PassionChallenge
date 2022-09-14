47fbf4f997ed68ea9b9595ef2d94994a260af8f1
miggeLucas <miggelucas@Mig-MacBook-Pro.local>
2022-09-14 14:13:25 -0300

    Ajeitando commit anterior

--- PassionChallenge/Views/RecomendationCollectionViewCell.swift
+++ PassionChallenge/Views/RecomendationCollectionViewCell.swift
@@ -29,15 +29,15 @@ class RecomendationCollectionViewCell: UICollectionViewCell {
 //        setupViewHierarchy()
 //        setupViewAttributes()
 //        setupConstraints()
-//        
+//
 //    }
 //    required init?(coder: NSCoder) {
 //            fatalError("init(coder:) has not been implemented")
 //    }
-//    
-//    
-//    
+//
+//
+//
     
     
 
-}
+
